315cf49085e85cd216b1d93e783218885b84354f
Jason Etcovitch <jasonetco@github.com>
2018-08-05 11:17:26 -0400

    Fix linting errors

--- browser/bin/package.js
+++ browser/bin/package.js
@@ -5,11 +5,10 @@ const path = require('path')
 
 const zip = new Zip()
 
-// Dummy, just compress the files, no control at all;
 files.forEach(file => {
   zip.file(file, fs.readFileSync(path.join(__dirname, '..', file), 'utf8'))
 })
 
-const data = zip.generate({ base64:false, compression:'DEFLATE' })
+const data = zip.generate({ base64: false, compression: 'DEFLATE' })
 fs.writeFileSync(path.join(__dirname, '..', 'probot-ui.zip'), data, 'binary')
 console.log('📦 Your package has been delivered!')
